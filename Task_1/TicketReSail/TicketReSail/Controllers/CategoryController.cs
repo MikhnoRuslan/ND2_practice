b2c3ceb1263808ce01e5ca9bd81de67331458f76
Ruslan Mikhno <r.mikhno92@gmail.com>
2020-11-16 16:15:07 +0300

    add index view category

--- Task_1/TicketReSail/TicketReSail/Controllers/CategoryController.cs
+++ Task_1/TicketReSail/TicketReSail/Controllers/CategoryController.cs
@@ -10,49 +10,15 @@ namespace TicketReSail.Controllers
     public class CategoryController : Controller
     {
         private readonly ICategoryService _categoryService;
-        private readonly IAction<CategoryDTO, Category> _action;
 
-        public CategoryController(ICategoryService categoryService, IAction<CategoryDTO, Category> action)
+        public CategoryController(ICategoryService categoryService)
         {
             _categoryService = categoryService;
-            _action = action;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _categoryService.GetCategories());
-        }
-
-        public IActionResult CreateCategory()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateCategory(CategoryViewModel categoryView)
-        {
-            if (ModelState.IsValid)
-            {
-                var categoryDto = new CategoryDTO { Name = categoryView.Name };
-                var operationDetails = await _action.Create(categoryDto);
-                if (operationDetails.Succeeded)
-                    return RedirectToAction("Index");
-                else
-                {
-                    ModelState.AddModelError(operationDetails.Property, operationDetails.Message);
-                    return View();
-                }
-            }
-
-            return NotFound();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> DeleteCategory(int id)
-        {
-            await _action.Delete(id);
-
-            return RedirectToAction("Index");
         }
     }
 }
