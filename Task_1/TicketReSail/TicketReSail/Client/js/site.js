f87cb1e85d8c627e23e63ad4840aa81c895f5f51
Ruslan Mikhno <r.mikhno92@gmail.com>
2020-12-07 00:02:27 +0300

    done

--- Task_1/TicketReSail/TicketReSail/Client/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿//import 'bootstrap';
-import 'jquery';
-//import 'bootstrap-select';
-
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-select/dist/css/bootstrap-select.css';
-
-import '../css/site.css';
